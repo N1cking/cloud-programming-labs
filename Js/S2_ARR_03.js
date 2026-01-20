8a6c361fafec0319a3c25686154ba121a4bb706c
N1cking <nicatkapoeira@gmail.com>
2026-01-20 21:34:26 +0100

    TASK13: S2_ARR_03 - Chunk

---
+++ Js/S2_ARR_03.js
@@ -0,0 +1,12 @@
+function chunk(arr, size) {
+    if (size <= 0) return null;
+    const result = [];
+    for (let i = 0; i < arr.length; i += size) {
+        result.push(arr.slice(i, i + size));
+    }
+    return result;
+}
+
+console.log(chunk([1, 2, 3, 4, 5, 6], 2));  // [[1, 2], [3, 4], [5, 6]]
+console.log(chunk([1, 2, 3, 4, 5, 6], 3));  // [[1, 2, 3], [4, 5, 6]]
+console.log(chunk([1, 2, 3, 4], 5));        // [[1, 2, 3, 4]]
