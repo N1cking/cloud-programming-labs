3edcc8f9554ec7857a21f40d1ac7f9de215713cd
N1cking <nicatkapoeira@gmail.com>
2026-01-20 22:08:44 +0100

    TASK29: S3_FN_01 - Convert to arrows

---
+++ Js/S3_FN_01.js
@@ -0,0 +1,7 @@
+const square = n => n * n;
+const isOdd = n => n % 2 !== 0;
+const greet = name => `Hello, ${name}`;
+
+console.log(square(4));      // 16
+console.log(isOdd(3));       // true
+console.log(greet('Alice')); // "Hello, Alice"
