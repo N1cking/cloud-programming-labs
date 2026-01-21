bedc6af8e7acbcf8bd9e4ad79f8d4aa89fcb704c
N1cking <nicatkapoeira@gmail.com>
2026-01-21 20:04:57 +0100

    TASK2: S1_VAR_02 - Rebinding and dynamic typing

---
+++ Python/Stage 1/S1_VAR_02.py
@@ -0,0 +1,14 @@
+x = 42
+print(x, type(x).__name__)
+
+x = "Hello"
+print(x, type(x).__name__)
+
+x = [1, 2, 3]
+print(x, type(x).__name__)
+
+x = True
+print(x, type(x).__name__)
+
+# Dynamic typing explanation
+print("\nDynamic typing means that the type of a variable can change during runtime.")
