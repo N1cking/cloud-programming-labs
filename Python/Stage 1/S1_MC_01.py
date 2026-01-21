f93cb4c02747dd85c65d640e9cca270f118a7f53
N1cking <nicatkapoeira@gmail.com>
2026-01-21 20:19:03 +0100

    TASK1C: S1_MC_01 - Day name

---
+++ Python/Stage 1/S1_MC_01.py
@@ -0,0 +1,22 @@
+def day_name(n: int):
+    match n:
+        case 1:
+            return "Monday"
+        case 2:
+            return "Tuesday"
+        case 3:
+            return "Wednesday"
+        case 4:
+            return "Thursday"
+        case 5:
+            return "Friday"
+        case 6:
+            return "Saturday"
+        case 7:
+            return "Sunday"
+        case _:
+            return None
+
+
+for n in [0, 1, 4, 7, 8]:
+    print(n, day_name(n))
