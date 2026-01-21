470fb36fe01b6ffa6fe30848d7436faae9858779
N1cking <nicatkapoeira@gmail.com>
2026-01-21 21:03:15 +0100

    TASK2: S2_LIST_03 - Chunk a list

---
+++ Python/Stage 2/S2_LIST_03.py
@@ -0,0 +1,13 @@
+def chunk(lst, size):
+    if size <= 0:
+        return None
+
+    result = []
+    for i in range(0, len(lst), size):
+        result.append(lst[i:i + size])
+    return result
+
+
+print(chunk([1, 2, 3, 4, 5], 2))
+print(chunk([1, 2, 3, 4, 5], 3))
+print(chunk([1, 2, 3], 0))
